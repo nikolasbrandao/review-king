27b3b17b32644b0b0ecdf7ada85ec409b634adf0
nikolasbrandao <nikolasbrandao@gmail.com>
2017-02-06 10:55:12 -0300

    added configurations in server.js

--- server/server.js
+++ server/server.js
@@ -10,3 +10,11 @@ var cors = require('cors');                      //Cross Origin Resource Sharing
 
 // configuration
 
+mongoose.connect('mongodb://localhost/reviewking');
+
+app.use( morgan.dev() ); //log every request to the console
+app.use( bodyParse.urlencoded({'extended':'true'}) ); //parse application/x-www-form-urlencoded
+app.use( bodyParse.json() ); //pase application/json
+app.use( bodyParse.json({ type: 'application/vnd.api+json' }) );
+app.use( methodOverride() );
+app.use( cors() );
