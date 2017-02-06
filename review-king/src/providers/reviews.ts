e09e7a31f1a6e1824c45f72fc7e7523eccb01630
nikolasbrandao <nikolasbrandao@gmail.com>
2017-02-06 14:10:19 -0300

    update reviews provider w/ get, create and delete

--- review-king/src/providers/reviews.ts
+++ review-king/src/providers/reviews.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
@@ -11,8 +11,41 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Reviews {
 
+  data: any;
+
   constructor(public http: Http) {
-    console.log('Hello Reviews Provider');
+    this.data = null;
+  }
+
+  getReviews(){
+    if(this.data){
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      
+      this.http.get('http://localhost:8080/api/reviews').map(res=>res.json()).subscribe(data => {
+        this.data = data;
+        resolve(this.data);
+      });
+    });
+  }
+
+  createReview(review){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    this.http.post('http://localhost:8080/api/reviews', JSON.stringify(review), {headers: headers})
+    .subscribe(res => {
+      console.log(res.json());
+    });
+  }
+
+  deleteReview(id){
+    this.http.delete('http://localhost:8080/api/reviews/' + id ).subscribe(res=>
+    {
+      console.log(res.json());     
+    });
   }
 
 }
